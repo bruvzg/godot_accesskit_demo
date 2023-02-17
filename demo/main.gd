05831d779283f490cb4a77e7e9906ef80ad3924e
bruvzg <7645683+bruvzg@users.noreply.github.com>
2023-02-17 20:06:21 +0200

    Add simple tree updates, and button focus following.

--- demo/main.gd
+++ demo/main.gd
@@ -1,6 +1,20 @@
 extends Control
 
 func _ready():
-	#var iface = DisplayServerExtensionManager.find_interface("AccessKit")
-	#iface.update_tree(DisplayServer.MAIN_WINDOW_ID, "TODO")
-	pass
+	var iface = DisplayServerExtensionManager.find_interface("AccessKit")
+	iface.update_tree(DisplayServer.MAIN_WINDOW_ID, """{
+			\"nodes\":[
+				{\"id\":1,\"role\":\"window\",\"name\":\"Hello from Godot\",\"children\":[2,3]},
+				{\"id\":2,\"role\":\"button\",\"name\":\"Button A\"},
+				{\"id\":3,\"role\":\"button\",\"name\":\"Button B\"}
+			],\"focus\":2
+		}""")
+	$Button1.grab_focus()
+
+func _on_button_1_focus_entered():
+	var iface = DisplayServerExtensionManager.find_interface("AccessKit")
+	iface.update_tree(DisplayServer.MAIN_WINDOW_ID, "{\"nodes\":[],\"focus\":2}")
+
+func _on_button_2_focus_entered():
+	var iface = DisplayServerExtensionManager.find_interface("AccessKit")
+	iface.update_tree(DisplayServer.MAIN_WINDOW_ID, "{\"nodes\":[],\"focus\":3}")
