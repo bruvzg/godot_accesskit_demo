f0bb4dd68522af33ffe8c204384ebf9fe380155a
bruvzg <7645683+bruvzg@users.noreply.github.com>
2023-02-22 14:29:31 +0200

    Add control bounds.

--- demo/main.gd
+++ demo/main.gd
@@ -5,15 +5,28 @@ func _ready():
 	iface.action_signal.connect(_action_handler)
 	iface.update_tree(DisplayServer.MAIN_WINDOW_ID, """{
 			\"nodes\":[
-				[1, {\"role\":\"window\",\"name\":\"Hello from Godot\",\"children\":[2,3,4]}],
-				[2, {\"role\":\"button\",\"name\":\"Button A\",\"defaultActionVerb\":\"click\"}],
-				[3, {\"role\":\"button\",\"name\":\"Button B\",\"defaultActionVerb\":\"click\"}],
-				[4, {\"role\":\"staticText\",\"name\":\"Test text.\",\"live\":\"polite\"}]
+				[1, {\"role\":\"window\",\"name\":\"Hello from Godot\",\"children\":[2,3,4],\"bounds\":%s}],
+				[2, {\"role\":\"button\",\"name\":\"Button A\",\"defaultActionVerb\":\"click\",\"bounds\":%s}],
+				[3, {\"role\":\"button\",\"name\":\"Button B\",\"defaultActionVerb\":\"click\",\"bounds\":%s}],
+				[4, {\"role\":\"staticText\",\"name\":\"Test text.\",\"live\":\"polite\",\"bounds\":%s}]
 			],
 			\"focus\":2
-		}""")
+		}""" % [
+			_make_rect(Rect2(Vector2(), get_window().size)),
+			_make_rect(Rect2($Button1.global_position, $Button1.size)),
+			_make_rect(Rect2($Button2.global_position, $Button2.size)),
+			_make_rect(Rect2($Label.global_position, $Label.size))
+		])
 	$Button1.grab_focus()
-	
+
+func _make_rect(rect):
+	var rect_dict = {}
+	rect_dict["x0"] = rect.position.x
+	rect_dict["y0"] = rect.position.y
+	rect_dict["x1"] = rect.position.x + rect.size.x
+	rect_dict["y1"] = rect.position.y + rect.size.y
+	return JSON.stringify(rect_dict)
+
 func _action_handler(json_data):
 	# Note: Use deferred call to ensure it is executed in the main thread
 	var data = JSON.parse_string(json_data) 
